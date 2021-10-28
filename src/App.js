ca247511e910a2c98bf97b74518c27bc842aaa79
Andrii Khomenko <khomenkoam007@gmail.com>
2021-10-28 22:55:24 +0300

    The 1st step. 2 buttons & displaying the result

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+ 
+  const [likes, setLikes] = useState(0)
+
+  function increase() {
+    setLikes(likes + 1)
+  }
+
+  function decrease() {
+    setLikes(likes - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Likes: {likes}</h1>
+      <button onClick={increase}>Increment</button>    
+      <button onClick={decrease}>Decrement</button>    
     </div>
   );
 }
