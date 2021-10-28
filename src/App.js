e0e673fe55de53da487eb388254852508f602d76
Andrii Khomenko <khomenkoam007@gmail.com>
2021-10-28 23:17:22 +0300

    two-way binding for example of input

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 function App() {
  
   const [likes, setLikes] = useState(0)
+  const [inputText, setText] = useState('')
 
   function increase() {
     setLikes(likes + 1)
@@ -16,7 +17,13 @@ function App() {
     <div className="App">
       <h1>Likes: {likes}</h1>
       <button onClick={increase}>Increment</button>    
-      <button onClick={decrease}>Decrement</button>    
+      <button onClick={decrease}>Decrement</button>
+      <h1>Text from input: {inputText}</h1>
+      <input 
+        type='text'
+        value={inputText}
+        onChange={e => setText(e.target.value)}
+         />    
     </div>
   );
 }
